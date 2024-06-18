633058065ec0e3f7d1aae88bcd27e5f43305bcb6
Dario Puddinu <dario.puddinu@abinsula.com>
2024-06-18 23:56:53 +0200

    style: edit theme screens config, style datepicker

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,6 +4,12 @@ import defaultTheme from 'tailwindcss/defaultTheme';
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px'
+    },
     extend: {
       fontFamily: {
         sans: ['Inter', ...defaultTheme.fontFamily.sans]
