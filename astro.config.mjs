1e2118536123ecd4dbdc7e7ffb8e2cb43bb38a14
Dario Puddinu <dario.puddinu@abinsula.com>
2024-07-06 16:46:37 +0200

    chore: remove pwa dependency

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,12 @@ import { defineConfig } from 'astro/config';
 import auth from "auth-astro";
 import vercel from "@astrojs/vercel/serverless";
 import tailwind from '@astrojs/tailwind';
-import AstroPWA from '@vite-pwa/astro';
 
 import db from "@astrojs/db";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), auth(), db(), AstroPWA()],
+  integrations: [tailwind(), auth(), db()],
   output: "server",
   adapter: vercel()
 });
