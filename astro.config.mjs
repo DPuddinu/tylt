6f73d22b096835c1a7624f09d4ff24d60dca7810
Dario Puddinu <dario.puddinu@abinsula.com>
2024-07-06 13:03:59 +0200

    feat: add pwa integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,13 @@ import { defineConfig } from 'astro/config';
 import auth from "auth-astro";
 import vercel from "@astrojs/vercel/serverless";
 import tailwind from '@astrojs/tailwind';
+import AstroPWA from '@vite-pwa/astro';
 
 import db from "@astrojs/db";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), auth(), db()],
+  integrations: [tailwind(), auth(), db(), AstroPWA()],
   output: "server",
   adapter: vercel()
 });
