4192d6a338187a62fe39f9ac32f3e74e54c8b1bf
Dario Puddinu <dario.puddinu@abinsula.com>
2024-08-02 00:05:35 +0200

    chore: switch to netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,21 @@
-import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel/serverless';
-import { defineConfig } from 'astro/config';
-import auth from 'auth-astro';
 import db from '@astrojs/db';
 import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind';
 import compress from 'astro-compress';
+import icon from 'astro-icon';
+import { defineConfig } from 'astro/config';
+import auth from 'auth-astro';
 import { VitePWA } from 'vite-plugin-pwa';
 import { manifest, seoConfig } from './src/utils/seoConfig.ts';
 
-import icon from "astro-icon";
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
   site: seoConfig.baseURL,
   integrations: [tailwind(), auth(), db(), sitemap(), compress(), icon()],
   output: 'server',
-  adapter: vercel(),
+  adapter: netlify(),
   prefetch: {
     prefetchAll: false,
     defaultStrategy: 'hover'
