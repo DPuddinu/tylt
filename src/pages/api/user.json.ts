1d199388f691c5791d0f54eeb5113b3dc6953bb4
Dario Puddinu <dario.puddinu@abinsula.com>
2024-08-11 15:27:24 +0200

    test: netlify cache dynamic response

---
+++ src/pages/api/user.json.ts
@@ -0,0 +1,27 @@
+import type { APIContext } from 'astro';
+
+export async function GET(context: APIContext) {
+  const user = context.locals.user;
+  // const product = await getActivities(id);
+
+  // if (!product) {
+  //   return new Response(null, {
+  //     status: 404,
+  //     statusText: 'Not found'
+  //   });
+  // }
+
+  return new Response(
+    JSON.stringify({
+      user
+    }),
+    {
+      status: 200,
+      headers: {
+        'Content-Type': 'application/json',
+        'Netlify-CDN-Cache-Control': 'public, durable, max-age=3600, stale-while-revalidate=120',
+        'Cache-Control': 'public, max-age=0, must-revalidate'
+      }
+    }
+  );
+}
