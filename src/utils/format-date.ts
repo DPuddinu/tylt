fdc468163582184eed31578f03ecb2adc0155664
Dario Puddinu <dario.puddinu@abinsula.com>
2024-12-30 15:51:08 +0100

    fix: format date

--- src/utils/format-date.ts
+++ src/utils/format-date.ts
@@ -1,5 +1,5 @@
-export function formatDate(date: Date | string, locale?: string): string {
-  return new Date(date).toLocaleDateString(locale ?? 'en-US', {
+export function formatDate(date: Date | string): string {
+  return new Date(date).toLocaleDateString('en-US', {
     year: 'numeric',
     month: 'short',
     day: 'numeric'
